aad523805eadda82498ec6482f593e306c9f999c
ahmed-fathy-forever-alone <unmei17@gmail.com>
2014-03-03 16:28:44 +0200

    testing

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,7 +7,6 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 
 import java.awt.Font;
-
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.GroupLayout;
